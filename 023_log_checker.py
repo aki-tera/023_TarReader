266793cfacea85ed7ef2c078a3b582acadae5595
aki-tera <aki.tera@gmail.com>
2021-09-21 19:25:13 +0900

    Ver0.5 feat:add getmembers

--- 023_log_checker.py
+++ 023_log_checker.py
@@ -15,15 +15,11 @@ while True:
 class TarReader:
     log = logger.Logger("TarReader", level=DEBUG_LEVEL)
 
-    def __init__(self, file_main, file_compressed_1, file_compressed_2=None, mode="utf-8"):
+    def __init__(self, file_main, file_compressed_1=None, file_compressed_2=None, mode="utf-8"):
         self.file_main = file_main
-        self.log.debug(self.file_main)
         self.file_compressed_1 = file_compressed_1
-        self.log.debug(self.file_compressed_1)
         self.file_compressed_2 = file_compressed_2
-        self.log.debug(self.file_compressed_2)
         self.mode = mode
-        self.log.debug(self.mode)
 
         self._tar = None
         self._tar1 = None
@@ -61,6 +57,7 @@ class TarReader:
                     self._tar3 = tarfile.open(fileobj=self._tar2)
 
                     for ii in self._tar3.getmembers():
+
                         self.log.debug(ii)
                         if self.file_compressed_2 in ii.name:
                             
@@ -93,28 +90,80 @@ class TarReader:
         temp = self._tar.readlines()
         for i in temp:
             result.append(i.rstrip().decode(self.mode))
-
         return result
+
+    def getmembers(self):
+        self.log.info(f"file_main:{self.file_main}")
+        self.log.info(f"file_compressed_1:{self.file_compressed_1}")
+        self.log.info(f"file_compressed_2:{self.file_compressed_2}")
+        result1 = []
+        resutl2 = []
+
+        # tarファイルの読み出し
+        self._tar1 = tarfile.open(self.file_main)
+
+        # tarファイル内がネストしているか確認する
+        if self.file_compressed_1 is None:
+
+            # ネストしていない場合
+            for i in self._tar1.getmembers():
+                result1.append(i.name)
+
+            # 終了処理
+            self._tar1.close()
+            self.log.info(f"result1:{result1}")
+
+            return result1
+
+        else:
+            # ネストしている場合
+            for i in self._tar1.getmembers():
+
+                result1.append(i.name)
+
+                if self.file_compressed_1 in i.name:
+                    # ネストしているファイル内を読み込む
+                    self._tar2 = self._tar1.extractfile(i)
+                    self._tar3 = tarfile.open(fileobj=self._tar2)
+
+                    for ii in self._tar3.getmembers():
+                        resutl2.append(ii.name)
+
+            # 終了処理
+            self._tar3.close()
+            self._tar2.close()
+            self._tar1.close()
+
+            self.log.info(f"result1:{result1}")
+            self.log.info(f"result2:{resutl2}")
+
+            # 2次元リストとして値を戻す
+            return [result1, resutl2]
 
 
 def main():
+    # 使い方の例
     # まとめて読み込む
     with TarReader("aaa.tgz", "bbb.tgz", "d.txt") as t:
         print(t.read())
 
     # 10文字分を読み込む
-    with TarReader("aaa.tgz", "bbb.tgz", "d.txt") as t:
+    with TarReader("aaa.tgz", "bbb.tgz", "c.txt") as t:
         print(t.read(10))
 
     # readlineで読み込む
-    with TarReader("aaa.tgz", "bbb.tgz", "d.txt") as t:
+    with TarReader("aaa.tgz", "b.txt") as t:
         while (result := t.readline()):
             print(result)
     
     # readlinesで読み込む
-    with TarReader("aaa.tgz", "bbb.tgz", "d.txt") as t:
+    with TarReader("aaa.tgz", "a.txt", ) as t:
         result = t.readlines()
         print(result)
+
+    # tar内のファイル一覧
+    result = TarReader("aaa.tgz", "bbb.tgz").getmembers()
+    print(result)
 
 
 if __name__ == "__main__":
